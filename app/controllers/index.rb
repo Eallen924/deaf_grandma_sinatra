bd7be9b9196e8032ba224420d77df9110cdaa160
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-15 15:37:27 -0500

    change redirect

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,5 @@
 get '/' do
+  p params
   @grandma = params[:grandma]
   # Look in app/views/index.erb
   erb :index
@@ -7,8 +8,9 @@ end
 post '/grandma' do
   input = params[:user_input]
   if input == input.upcase
-    @grandma = "No, not since 1983!"
+    grandma = "No, not since 1983!"
   else
-    @grandma = "HUH?!?! SPEAK UP!"
+    grandma = "HUH?!?! SPEAK UP!"
   end
+  redirect "/?grandma=#{grandma}"
 end
